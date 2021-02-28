06de21e95153e8776b880405c157010defd26d36
Michael Janum Davidsen <au600586@uni.au.dk>
2021-02-28 17:07:03 +0100

    still crashing, still compilable

--- app/src/main/java/com/example/cityweatherapp/DetailsActivity.java
+++ app/src/main/java/com/example/cityweatherapp/DetailsActivity.java
@@ -6,67 +6,131 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    String cityData, weatherData, notesData;
+    double tempData, humidityData, ratingData;
+    int image, position;
+    ImageView flag;
+    TextView city, temp, humidity, weather, rating, notes;
+    Button editBtn, backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_activity);
 
-       ///Intent
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("position", 0);
-
         //Widgets
-        ImageView flag = findViewById(R.id.flag_details);
-        TextView city = findViewById(R.id.city_details);
-        TextView temp = findViewById(R.id.temp_details);
-        TextView humidity = findViewById(R.id.humidity_details);
-        TextView weather = findViewById(R.id.weather_details);
-        TextView rating = findViewById(R.id.rating_details);
-        TextView notes = findViewById(R.id.notes_field);
+        flag = findViewById(R.id.flag_details);
+        city = findViewById(R.id.city_details);
+        temp = findViewById(R.id.temp_details);
+        humidity = findViewById(R.id.humidity_details);
+        weather = findViewById(R.id.weather_details);
+        rating = findViewById(R.id.rating_details);
+        notes = findViewById(R.id.notes_field);
+        editBtn = findViewById(R.id.editBtn);
+        backBtn = findViewById(R.id.backBtn);
 
-        //Get the correct flag from drawable directory
-        int id = getResources().getIdentifier(
-                ListActivity.weatherData.get(position).getCountry().toLowerCase(),
-                "drawable",
-                getPackageName()
-        );
-        flag.setImageResource(id);
-
-        city.setText(ListActivity.weatherData.get(position).getCity());
-        temp.setText(ListActivity.weatherData.get(position).getTemp() + " Celsius");
-        humidity.setText(ListActivity.weatherData.get(position).getHumidity() + " %");
-        weather.setText(ListActivity.weatherData.get(position).getWeather());
-        rating.setText(ListActivity.weatherData.get(position).getRating() + "");
-        notes.setText(ListActivity.weatherData.get(position).getNote());
+        if(savedInstanceState != null) {
+            cityData = savedInstanceState.getString("City");
+            tempData = savedInstanceState.getDouble("Temp");
+            weatherData = savedInstanceState.getString("Weather");
+            humidityData = savedInstanceState.getDouble("Humidity");
+            ratingData = savedInstanceState.getDouble("Rating", 0);
+            notesData = savedInstanceState.getString("Notes");
+            image = savedInstanceState.getInt("imagesArray");
+        } else {
+            getData();
+        }
+        setData();
 
 
         //Back button function bind - rolls backs to the list activity
-        Button backBtn = findViewById(R.id.backBtn);
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), ListActivity.class);
-                startActivity(intent);
-            }
+        backBtn.setOnClickListener(f -> {
+                Intent resultIntent = getIntent();
+                resultIntent.putExtra("Position", position);
+                resultIntent.putExtra("Notes", notesData);
+                resultIntent.putExtra("Rating", ratingData);
+                setResult(RESULT_OK, resultIntent);
+                finish();
         });
+
 
         //Edit button, with intent to start new acitivity
-
-        Button editBtn = findViewById(R.id.editBtn);
-        editBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), EditActivity.class);
-                intent.putExtra("position", position);
-                startActivity(intent);;
-            }
+        editBtn.setOnClickListener(f -> {
+                Intent intent = new Intent(DetailsActivity.this, EditActivity.class);
+                intent.putExtra("City", cityData);
+                intent.putExtra("Temp", tempData);
+                intent.putExtra("Weather", weatherData);
+                intent.putExtra("Humidity", humidityData);
+                intent.putExtra("imagesArray", image);
+                intent.putExtra("Rating", ratingData);
+                intent.putExtra("Notes", notesData);
+                DetailsActivity.this.startActivityForResult(intent, 666);
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("City", cityData);
+        savedInstanceState.putString("Notes", notesData);
+        savedInstanceState.putDouble("Temp", tempData);
+        savedInstanceState.putDouble("Rating", ratingData);
+        savedInstanceState.putString("Weather", weatherData);
+        savedInstanceState.putDouble("Humidity", humidityData);
+        savedInstanceState.putInt("imagesArray", image);
+        savedInstanceState.putInt("Position", position);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private void getData() {
+        if(getIntent().hasExtra("City") && getIntent().hasExtra("Temp") && getIntent().hasExtra("Weather") && getIntent().hasExtra("Humidity") && getIntent().hasExtra("imagesArray"))
+        {
+            cityData = getIntent().getExtras().getString("City");
+            tempData = getIntent().getExtras().getDouble("Temp",0);
+            weatherData = getIntent().getExtras().getString("Weather");
+            humidityData = getIntent().getExtras().getDouble("Humidity", 1);
+            image = getIntent().getExtras().getInt("imagesArray",1);
+            ratingData = getIntent().getExtras().getDouble("Rating",1);
+            position = getIntent().getExtras().getInt("Position",1);
+            notesData = getIntent().getExtras().getString("Notes");
+        }
+        else
+        {
+            Toast.makeText(this,"Intent Extra is empty, can't load in data", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 666) {
+            if (resultCode == RESULT_OK)
+            {
+                //Toast.makeText(this,"Inside OnActivityRes", Toast.LENGTH_SHORT).show();
+                ratingData = data.getDoubleExtra("Rating",1);
+                notesData = data.getStringExtra("Notes");
+                notes.setText(notesData);
+                rating.setText(String.format("User Rating:  %s", ratingData));
+            }
+        }
+    }
+
+    private void setData() {
+        city.setText(cityData);
+        temp.setText(String.format("Temp:  %sC", tempData));
+        weather.setText(String.format("Weather: %s", weatherData));
+        humidity.setText(String.format("Humidity: %s%%", humidityData));
+        flag.setImageResource(image);
+        notes.setText(notesData);
+        rating.setText(String.format("User Rating:  %s", ratingData));
+    }
+
 }
--- app/src/main/java/com/example/cityweatherapp/WeatherAdapter.java
+++ app/src/main/java/com/example/cityweatherapp/WeatherAdapter.java
@@ -2,7 +2,6 @@ package com.example.cityweatherapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,56 +12,48 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.MyViewHolder> {
     Context ctx;
-    private List<WeatherSample> weatherData = new ArrayList<>();
-    //private itemClickListener onClickListener;
+    List<WeatherSample> weatherData;
+    int[] imagesArray;
 
-    public WeatherAdapter(Context ctx, List<WeatherSample> weatherData) {
+    public WeatherAdapter(Context ctx, List<WeatherSample> weatherData, int[] images) {
         this.ctx = ctx;
         this.weatherData = weatherData;
-
-        //this.onClickListener = onClickListener;
+        imagesArray = images;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         LayoutInflater inflater = LayoutInflater.from(ctx);
         View view = inflater.inflate(R.layout.row_data, parent, false);
         return new MyViewHolder(view);
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.country.setText(weatherData.get(position).getCountry());
+        holder.city.setText(weatherData.get(position).getCity());
         holder.weather.setText(weatherData.get(position).getWeather());
-        holder.temp.setText(weatherData.get(position).getTemp() + " Celsius");
-        holder.rating.setText("" + weatherData.get(position).getRating());
+        holder.temp.setText(String.format("%s C", weatherData.get(position).getTemp()));
+        holder.rating.setText(String.format("%s", weatherData.get(position).getRating()));
+        holder.flag.setImageResource(imagesArray[position]);
 
-        int id = ctx.getResources().getIdentifier(
-                weatherData.get(position).getCountry().toLowerCase(),
-                "drawable",
-                ctx.getPackageName()
-        );
-        holder.flag.setImageResource(id);
-        holder.aclayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        holder.list_activity_layout.setOnClickListener(f -> {
                 Intent intent = new Intent(ctx, DetailsActivity.class);
                 intent.putExtra("City", weatherData.get(position).getCity());
                 intent.putExtra("Country", weatherData.get(position).getCountry());
                 intent.putExtra("Humidity", weatherData.get(position).getHumidity());
-                intent.putExtra("Note", weatherData.get(position).getNote());
+                intent.putExtra("Notes", weatherData.get(position).getNote());
                 intent.putExtra("Temp", weatherData.get(position).getTemp());
                 intent.putExtra("Rating", weatherData.get(position).getRating());
                 intent.putExtra("Weather", weatherData.get(position).getWeather());
-                ctx.startActivity(intent);
-            }
+                intent.putExtra("Position", position);
+
+                ((ListActivity) ctx).startActivityForResult(intent, 666);
         });
     }
 
@@ -71,33 +62,23 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.MyViewHo
         return weatherData.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView country, weather, temp, rating;
+        TextView city, weather, temp, rating;
         ImageView flag;
-
-        /*itemClickListener onClickListener;*/
-
-        ConstraintLayout aclayout;
+        ConstraintLayout list_activity_layout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            country = itemView.findViewById(R.id.city);
-            temp = itemView.findViewById(R.id.temp);
+            city = itemView.findViewById(R.id.city);
             weather = itemView.findViewById(R.id.weather);
+            temp = itemView.findViewById(R.id.temp);
             rating = itemView.findViewById(R.id.rating);
             flag = itemView.findViewById(R.id.flag);
-            aclayout = itemView.findViewById(R.id.listactivityconstraint);
-
-            /*this.onClickListener = onClickListener;
-            itemView.setOnClickListener((View.OnClickListener) this);*/
+            list_activity_layout = itemView.findViewById(R.id.listactivityconstraint);
         }
-        /*public void onClick(View v) {
-            onClickListener.onItemClick(getAdapterPosition());*/
     }
 }
 
 
-    /*public interface itemClickListener {
-        void onItemClick(int position);*/
 
